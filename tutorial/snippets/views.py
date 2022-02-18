fb00352e11977f4a53ff68e02d4ef52b17257359
shyatu <67035613+shyatu@users.noreply.github.com>
2022-02-18 13:50:22 +0800

    Remove api_root due to using DefaultRouter() in urls.py

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,11 +1,8 @@
 from django.contrib.auth.models import User
 from rest_framework import permissions
-from rest_framework import generics
 from rest_framework import renderers
 from rest_framework import viewsets
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
 from rest_framework.decorators import action
 
 from snippets.models import Snippet
@@ -13,12 +10,14 @@ from snippets.permissions import IsOwnerOrReadOnly
 from snippets.serializers import SnippetSerializer, UserSerializer
 
 
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format)
-    })
+# from rest_framework.decorators import api_view
+# from rest_framework.reverse import reverse
+# @api_view(['GET'])
+# def api_root(request, format=None):
+#     return Response({
+#         'users': reverse('user-list', request=request, format=format),
+#         'snippets': reverse('snippet-list', request=request, format=format)
+#     })
 
 
 class SnippetViewSet(viewsets.ModelViewSet):
